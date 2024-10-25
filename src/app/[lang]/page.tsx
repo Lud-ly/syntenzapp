3532cd071f860739093403085f4f6c227de1933f
Ludovic Mouly <ludom82@gmail.com>
2024-10-25 18:12:22 +0200

    👌 IMPROVE: nav

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -36,15 +36,15 @@ export default async function Home(
       {/* Header fixe avec fond flouté */}
       <header className="fixed top-0 w-full  z-50 shadow-sm">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <div className="flex items-center justify-between h-16">
+          <div className="flex items-center justify-between h-16 bg-teal-950 hover:bg-teal-900 rounded-lg">
             {/* Logo */}
-            <div className="flex-shrink-0 flex items-center space-x-2 bg-teal-950 hover:bg-teal-900 
+            <div className="flex-shrink-0 flex items-center space-x-2 
                  text-white px-4 py-4 rounded-lg ">
               <Image
                 src="/images/logo.webp"
                 alt="Logo"
-                width={60}
-                height={60}
+                width={48}
+                height={48}
                 className="object-contain"
                 priority
               />
